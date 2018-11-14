03aa6a77b7b55c2a66d8dbec7d98f7ef815ba240
Dominik Hofman <dominikhofman0@gmail.com>
2018-11-14 01:45:13 +0100

    Updated readme and added install scripts

--- install1.sh
+++ install1.sh
@@ -1,6 +1,6 @@
 
-apt-get update && apt-get upgrade -y
-apt-get install -y python3-pip git vim
-apt-get install -y pkg-config libboost-python-dev libboost-thread-dev libbluetooth-dev libglib2.0-dev python-dev
-ln -s /usr/lib/arm-linux-gnueabihf/libboost_python-py35.so /usr/lib/arm-linux-gnueabihf/libboost_python-py34.so
+apt-get update && apt-get upgrade -y && \
+apt-get install -y python3-pip git vim && \
+apt-get install -y pkg-config libboost-python-dev libboost-thread-dev libbluetooth-dev libglib2.0-dev python-dev && \
+ln -s /usr/lib/arm-linux-gnueabihf/libboost_python-py35.so /usr/lib/arm-linux-gnueabihf/libboost_python-py34.so && \
 sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1000/g' /etc/dphys-swapfile
